d8256a89b2f97602581f09ab9b286026744c55bc
Murilo Góes de Almeida <murilow@gmail.com>
2021-04-29 23:32:26 -0300

    subindo com random forest customizada

--- train_models.py
+++ train_models.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier
@@ -15,6 +16,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+
 
 import pickle
 from ConfusionMatrix import ConfusionMatrix
@@ -64,7 +67,8 @@ with open('data/todos_bos_contextos_tratados.csv','r') as csvfile:
         labels.append(row[1])
 
 # criando o vetor tf-idf
-vectorizer = TfidfVectorizer(use_idf=True)
+#vectorizer = TfidfVectorizer(use_idf=True)
+vectorizer = TfidfVectorizer(min_df=5, max_df=0.8)
 X = vectorizer.fit_transform(documents)
 y = labels
 
@@ -72,29 +76,42 @@ with open('data/vectorizer/vectorizer.pickle', 'wb') as f:
     pickle.dump(vectorizer, f)
 
 # aqui eu estou fazendo um undersampler para balancear o dataset
-rus = RandomUnderSampler()
-X, y = rus.fit_resample(X, y)
+#rs = RandomUnderSampler()
+# trocando para oversampler com rain forest ficou super bom
+rs = RandomOverSampler()
+X, y = rs.fit_resample(X, y)
 
 
 #mandando treinar
 
 # Naive Bayes
-#train(MultinomialNB(alpha=0.05), X, y, [0,1])
+# https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html
+# testei o alpha em diversos valores, o 0.5 ficou com uma precisao melhor (0.81)
+# Erro 0 249
+# erro 1 300
+#train(MultinomialNB(alpha=0.5), X, y, [0,1])
 
-# SVC
-#train(svm.SVC(kernel='linear', C=1.0), X,y, [0,1])
+#train(MultinomialNB(), X, y, [0,1])
+
 
 # Linear SVC
-#train(svm.LinearSVC(random_state=0, tol=1e-05), X,y, [0,1])
+# acuracia 0.80 nas esta acertando mais o 1 ... aumentando a interacao para 10mil chegou as vezes a 0.81
+#train(svm.LinearSVC(max_iter=10000), X,y, [0,1])
 
 # Random Forest
-#train(RandomForestClassifier(max_depth=2, random_state=0), X,y, [0,1])
+# parece que aprende mais sobre a classe 0 e a 1 erra muito
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+train(RandomForestClassifier(n_estimators=100), X,y, [0,1])
 
 # Stochastic Gradient Descent Classifier
-train(SGDClassifier(max_iter=1000, tol=1e-3), X,y, [0,1])
+#train(SGDClassifier(max_iter=1000, tol=1e-3), X,y, [0,1])
 
-# multilayer perceptron
+# multilayer perceptron demora muito
 #train(MLPClassifier(random_state=1, max_iter=300), X,y, [0,1])
+
+# SVC demora muito
+# train(svm.SVC(kernel='linear', C=1.0), X,y, [0,1])
+
 
 
 #tentar linearSvc SGDClassifier Random Forest  Multilayer Perceprtron (MLP)
